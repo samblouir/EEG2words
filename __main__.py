f177e98625bb61418e9fdfc623708d1d060a79a1
Samuel Blouir <scblouir@gmail.com>
2021-03-18 20:54:03 -0400

    Calculates accuracy per category. Open and sorts GloVe840 and GloVe42 with minimal RAM usage.

--- __main__.py
+++ __main__.py
@@ -1,16 +1,11 @@
-'''
-watch -n 0 'rsync -av /home/sam/PycharmProjects/EEG2words/ laura@192.168.1.15:/home/laura/PycharmProjects/EEG2words && sudo rsync -av laura@192.168.1.15:/home/laura/PycharmProjects/EEG2words/data/ /home/sam/PycharmProjects/EEG2words/data'
-'''
-
-from itertools import repeat
-import ujson as json
-from typing import List
-
-from shared_functions import init, load_et_data, load_eeg_data, print_dict, timerD, prepare_embedding_layer
-import numpy as np
+import multiprocessing as mp
 # import jax as jnp
 import os
-import multiprocessing as mp
+from itertools import repeat
+
+import numpy as np
+
+from shared_functions import timerD
 
 os.environ.setdefault('TF_CPP_MIN_LOG_LEVEL', '3')
 import tensorflow as tf
@@ -18,6 +13,14 @@ from tensorflow.python.layers.base import Layer
 
 for gpu in tf.config.list_physical_devices("GPU"):
     tf.config.experimental.set_memory_growth(gpu, True)
+
+
+def get_analogy_file_path():
+    return get_file(URL="https://raw.githubusercontent.com/nicholas-leonard/word2vec/master/questions-words.txt")
+
+
+def open_analogy_file():
+    return open(get_analogy_file_path())
 
 
 def prepare_glove(name="6B"):
@@ -95,40 +98,128 @@ def prepare_arguments(*x):
     return zip(*args)
 
 
+def prep_embeddings_mp(glove_file, proc_count, N):
+    batch_size = int(len(glove_file) / N)
+    lines = glove_file[proc_count * batch_size:(proc_count + 1) * batch_size]
+    words = ["" for _ in range(len(lines))]
+    vectors = [[0.0 for __ in range(len(lines[0].split()) - 1)] for _ in range(len(lines))]
+    words, vectors = np.array(words, dtype=object), np.zeros_like(vectors, dtype=np.float32)
+
+    return words, np.squeeze(vectors)
+
+
+# Slices up the the glovefiles into separate files of words and vectors
+def prepare_embeddings_files(n=300, glove_size=6):
+    new_folder = f"data/glove/{glove_size}B.{n}d"
+    os.system(f"mkdir -p {new_folder}")
+    vector_path = f"{new_folder}/glove.{glove_size}B.{n}d.vectors"
+    word_path = f"{new_folder}/glove.{glove_size}B.300d.words"
+    glove_path = f"{new_folder}/glove.{glove_size}B.300d.txt"
+    # word_npy_path = f"{new_folder}/glove.{glove_size}B.{n}d.words.npy"
+    vector_npy_path = f"{new_folder}/glove.{glove_size}B.{n}d.vectors.npy"
+    word_path_sorted = f"{new_folder}/glove.{glove_size}B.300d.words_sorted"
+    vector_npy_path_sorted = f"{new_folder}/glove.{glove_size}B.{n}d.vectors_sorted.npy"
+
+    # print(len(open(glove_path).readlines()))
+    # exit()
+    ###################################################################################################
+    ###################################################################################################
+    if not os.path.exists(vector_path) or not os.path.exists(word_path):
+        fW = open(word_path, "w")
+        fV = open(vector_path, "w")
+        for line in open(glove_path):
+            data = line.split(' ')
+            print(data[0][0], end='\r')
+            word, vector = ''.join(data[0]), np.array(data[1:], dtype=np.float32)
+            fW.write(f"{word}\n")
+            for each in vector:
+                fV.write(f"{str(each)} ")
+            fV.write("\n")
+        timerD(f"Preparing {vector_path.rsplit('/', 1)[1]} and {word_path.rsplit('/', 1)[1]}")
+
+    # if not os.path.exists(word_npy_path):
+    #     timerD(f"Preparing {word_npy_path}")
+    #     word_lines = open(word_path).readlines()
+    #     word_array = ["" for _ in range(len(word_lines))]
+    #     for index, line in enumerate(word_lines):
+    #         print(f"{index / len(word_lines) * 100:2.1f}%", end='\r')
+    #         word_array[index] = np.array(line.split(), dtype=object)[0]
+    #     np.save(word_npy_path, word_array, allow_pickle=True)
+
+    if not os.path.exists(vector_npy_path):
+        vector_lines = open(vector_path).readlines()
+        vector_array = [[0.0 for __ in range(300)] for _ in range(len(vector_lines))]
+        for index, line in enumerate(vector_lines):
+            print(f"{index / len(vector_lines) * 100:2.1f}%", end='\r')
+            vector_array[index] = np.array(line.split(), dtype=np.float32)[:]
+        np.save(vector_npy_path, vector_array, allow_pickle=True)
+        timerD(f"Preparing {vector_npy_path}")
+
+    if not os.path.exists(vector_npy_path_sorted) or not os.path.exists(word_path_sorted):
+        a = np.array(open(word_path).read().split())
+        b = np.load(vector_npy_path, allow_pickle=True)
+        a, b = zip([a, b])
+        a, b = np.concatenate([*a]), np.concatenate([*b])
+        a, b = zip(*sorted(((zip(a, b)))))
+        b = np.array(b, dtype=object)
+        # np.save(vector_npy_path_sorted, b, allow_pickle=True)
+
+        clean_b = []
+        fW = open(word_path_sorted, "w")
+        for index, (q, w) in enumerate(zip(a, b)):
+            if len(w) == 300:
+                fW.write(f"{q}\n")
+                clean_b.append(b[index])
+
+        np.save(vector_npy_path_sorted, clean_b, allow_pickle=True)
+        timerD(f"Sorting and saving to {word_path_sorted.rsplit('/', 1)[1]} and {vector_npy_path_sorted.rsplit('/', 1)[1]}")
+
+
 def load_embeddings(n=300, glove_size=6):
     new_folder = f"data/glove/{glove_size}B.{n}d"
     os.system(f"mkdir -p {new_folder}")
     vector_path = f"{new_folder}/glove.{glove_size}B.{n}d.vectors.npy"
-    word_path = f"data/glove/glove.{glove_size}B.300d.words.npy"
-    glove_path = f"data/glove/glove.{glove_size}B.300d.txt"
+    word_path = f"{new_folder}/glove.{glove_size}B.300d.words.npy"
+    word_path = f"{new_folder}/glove.{glove_size}B.300d.words"
+    glove_path = f"{new_folder}/glove.{glove_size}B.300d.txt"
+    word_path_sorted = f"{new_folder}/glove.{glove_size}B.300d.words_sorted"
+    vector_npy_path_sorted = f"{new_folder}/glove.{glove_size}B.{n}d.vectors_sorted.npy"
 
     ###################################################################################################
     ###################################################################################################
-    if not os.path.exists(vector_path) or not os.path.exists(word_path):
-        glove = open(glove_path).readlines()
+    if glove_size > 6:
+        prepare_embeddings_files(n=n, glove_size=glove_size)
+    else:
+        if not os.path.exists(vector_path) or not os.path.exists(word_path):
+            glove = open(glove_path).readlines()
 
-        process_count = mp.cpu_count()
-        args = zip(repeat(glove), range(process_count), repeat(process_count))
-        results = q_smp_N(mp_load_file_two_arrays2, args, process_count)
+            process_count = mp.cpu_count()
+            args = zip(repeat(glove), range(process_count), repeat(process_count))
+            results = q_smp_N(mp_load_file_two_arrays2, args, process_count)
 
-        # Combine MP results and split into word and vector lists
-        a, b = zip(*results)
-        a, b = np.concatenate([*a]), np.concatenate([*b])
-        a, b = zip(*sorted(((zip(a, b)))))
-        word_array, vector_array = np.array(a), np.array(b)
+            # Combine MP results and split into word and vector lists
+            a, b = zip(*results)
+            a, b = np.concatenate([*a]), np.concatenate([*b])
+            a, b = zip(*sorted(((zip(a, b)))))
+            word_array, vector_array = np.array(a), np.array(b)
 
-        if not os.path.exists(vector_path): np.save(vector_path, vector_array, allow_pickle=True)
-        if not os.path.exists(word_path): np.save(word_path, word_array, allow_pickle=True)
+            fW = open(word_path, "w")
+            for word in word_array:
+                fW.write(f"{word}\n")
+
+            if not os.path.exists(vector_npy_path_sorted): np.save(vector_npy_path_sorted, vector_array, allow_pickle=True)
+            if not os.path.exists(word_path_sorted): np.save(word_path_sorted, word_array, allow_pickle=True)
 
     ###################################################################################################
     ###################################################################################################
 
-    a = np.load(word_path, allow_pickle=True)
-    b = np.load(vector_path, allow_pickle=True)
+    a = np.array(open(word_path_sorted).read().split())
+    b = np.load(vector_npy_path_sorted, allow_pickle=True)
+
     # Create a dict with pointers to vector_list
     c = {str(a[idx]): b[idx] for idx in range(len(a))}
 
-    word_array, vector_array, word_vector_dict = np.array(a), np.array(b), c
+    word_array, vector_array, word_vector_dict = np.array(a, dtype=object), np.array(b), c
     return word_array, vector_array, word_vector_dict
 
 
@@ -198,7 +289,6 @@ def convert_to_tensors(curr, dtype=tf.float32):
 def calculate_analogy_vectors(in_dict, vector_list, limit=None):
     analogy_path = get_file(URL="https://raw.githubusercontent.com/nicholas-leonard/word2vec/master/questions-words.txt")
     file_path = "data/float32_analogy_vectors.npy"
-    # file_path = "data/float64_analogy_vectors.npy"
 
     ###################################################################################################
     ###################################################################################################
@@ -214,9 +304,11 @@ def calculate_analogy_vectors(in_dict, vector_list, limit=None):
     vectors = np.load(file_path, allow_pickle=True)
     vectors = vectors[:limit]  # Limit the amount of vectors, for debugging
     predicted_vectors: np.ndarray = vectors[:, 1] - vectors[:, 0] + vectors[:, 2]
+    convert_to_tensors(predicted_vectors)
+    convert_to_tensors(vector_list)
     currT, bT = q_mp(convert_to_tensors, predicted_vectors), convert_to_tensors(vector_list)
 
-    timerD("Finding data")
+    timerD("Converting data to tensors")
     tf_layer = AnalogyIndiceLayer()
     locs = list(map(lambda x: tf_layer((x, bT)).numpy(), currT))
 
@@ -248,14 +340,23 @@ def get_analogy_answers():
 if __name__ == '__main__':
     os.system("clear")
     timerD("Importing libraries")
+    # prepare_embeddings_files(glove_size=6)
+    # prepare_embeddings_files(glove_size=840)
+    # exit()
 
     words = ["car", "automobile", "truck", "bus", "limo", "jeep", "boat", "canoe", "dinghy", "motorboat", "yacht", "catamaran"]
 
     n, glove_size = 300, 6
+    # n, glove_size = 300, 42
+    # n, glove_size = 300, 840
     limit = None  # Reduces the amount of words done, for debugging. Set to None for the full dataset.
+
+    # n, glove_size = 300, 6
     # limit = 5
 
     word_list, vector_list, word_vector_dict = load_embeddings(n=n, glove_size=glove_size)
+    # print(word_list)
+    # exit()
 
     predicted_analogy_indices = calculate_analogy_vectors(word_vector_dict, vector_list, limit=limit)
     timerD(f"Predicting analogy indices")
@@ -268,15 +369,41 @@ if __name__ == '__main__':
     true_if_correct = q_smp(score_if_same, args)
     number_correct = np.sum(true_if_correct)
     percent_correct = f"{number_correct / len(predicted_analogies) * 100:2.1f}"
+    timerD(f"Checking accuracy. Results (correct/total): {number_correct} / {len(predicted_analogies)} ({percent_correct}%)")
+
+    analogies = open_analogy_file().readlines()
+
+    categories = []
+    for idx, each in enumerate(analogies):
+        if len(each.split()) != 4:
+            category_name = each.split()[1]
+            categories.append([category_name, idx])
+
+    categorized = []
+    for i in range(len(categories)):
+        if i < len(categories) - 1:
+            idx0, idx1 = categories[i][1], categories[i + 1][1]
+            t0, t1 = analogy_answers[idx0:idx1], predicted_analogies[idx0:idx1]
+        else:
+            idx0 = categories[i][1]
+            t0, t1 = analogy_answers[idx0:], predicted_analogies[idx0:]
+
+        args = zip(repeat(t0), repeat(t1), range(len(t0)))
+        number_correct = np.sum(q_smp(score_if_same, args))
+        percent_correct = f"{number_correct / len(t1) * 100:02.1f}"
+        category_name = categories[i][0]
+        print(f"{category_name:>30}, Accuracy: {percent_correct}% ({number_correct}/{len(t1)})")
+
+    # predictions_dir = f"data/predictions"
+    # os.system(f"mkdir -p {predictions_dir}")
+    # incorrect_predictions_path = f"{predictions_dir}/wrong_predictions-glove_{glove_size}-correct_{percent_correct}p.txt"
+    # with open(incorrect_predictions_path, 'w') as f:
+    #     for a, b, c in zip(predicted_analogies, analogy_answers, true_if_correct):
+    #         if not c:
+    #             pretty = f"{a:>20} | {b:<20}"
+    #             f.write(f"{pretty}\n")
+    #             # print(pretty)
+
     # timerD(f"Checking accuracy. Results (correct/total): {number_correct} / {len(predicted_analogies)} ({percent_correct}%)")
 
-    incorrect_predictions_path = f"data/wrong_predictions-{percent_correct}p.txt"
-    with open(incorrect_predictions_path, 'w') as f:
-        for a, b, c in zip(predicted_analogies, analogy_answers, true_if_correct):
-            if not c:
-                pretty = f"{a:>20} | {b:<20}"
-                f.write(f"{pretty}\n")
-                print(pretty)
-
-    timerD(f"Checking accuracy. Results (correct/total): {number_correct} / {len(predicted_analogies)} ({percent_correct}%)")
     timerD(f"Exiting...", show_total_time_elapsed=True)
